9671d0ad80c9797ba273a1dbdb00b0cb75bc3f68
Eduhh <frazaoe620@gmail.com>
2026-09-14 13:27:33 -0300

    Add initial Capacitor configuration file

---
+++ capacitor.config.ts
@@ -0,0 +1,13 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.app.floating',
+  appName: 'Floating Buttons',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
+
